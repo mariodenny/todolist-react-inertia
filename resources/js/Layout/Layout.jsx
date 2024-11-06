50c6b009da41529951ecd91ba4491a33781741bc
ITsDenny <dennym.channel@gmail.com>
2024-11-06 16:09:28 +0700

    feat(login) add login form to view

--- resources/js/Layout/Layout.jsx
+++ resources/js/Layout/Layout.jsx
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
                 <nav className="flex space-x-4">
                     <Link href="/" className="hover:text-gray-400">Home</Link>
                     <Link href="/todos" className="hover:text-gray-400">Todos</Link>
+                    <Link href="/user/login" className="hover:text-gray-400">Task assignment</Link>
                 </nav>
             </header>
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('todos', [TodoController::class, 'index']);
 Route::get('test', [TodoController::class, 'test']);
-Route::inertia('', 'Todos/Home');
+Route::get('user/login', [UserController::class, 'loginForm']);
 
 Route::prefix('todos')->controller(TodoController::class)->group(function () {
     Route::get('', 'index');
